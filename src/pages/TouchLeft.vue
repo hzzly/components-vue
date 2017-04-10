656d74a909fcc61498a8db474d8bf94e8b85e74f
黄敬仁 <hzzly@huangjirendeMBP.workgroup>
2017-04-10 23:24:11 +0800

    添加qq左滑删除

---
+++ src/pages/TouchLeft.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <touch-left></touch-left>
+    </div>    
+</template>
+
+<script>
+    import touchLeft from '../components/touchLeft/touchLeft.vue'
+    export default {
+        data() {
+            return {
+            }
+        },
+        components: {
+            touchLeft
+        }
+    }
+</script>

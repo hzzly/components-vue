7fb5f3077d7e9a45c35278a90b6e192b36bc25ac
黄敬仁 <hzzly@huangjirendeMBP.workgroup>
2017-04-10 23:25:38 +0800

    qq左滑删除组件

---
+++ src/components/touchLeft/touchLeft.vue
@@ -0,0 +1,99 @@
+<template>
+	<div>
+		<button @click="add" class="add-btn">+</button>
+		<transition-group name="slide" tag="ul" class="list-wrapper">
+			<li class="list" v-for="(item, index) in lists" :key="item">
+				<v-touch class="vuetouch" v-on:swipeleft="showBtn(index)" v-on:swiperight="hideBtn(index)">
+					<span class="text">{{item.text}}</span>
+					<transition name="move">
+						<button class="del-btn" @click="delList(index)" v-show="item.show">删除</button>
+					</transition>
+				</v-touch>
+				
+			</li>
+		</transition-group>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				lists: [
+					{ text: 'todo1', show: false },
+					{ text: 'todo1', show: false },
+					{ text: 'todo1', show: false },
+					{ text: 'todo1', show: false },
+				]
+			}
+		},
+		methods: {
+			add() {
+				let list = {
+					text: 'new todo', show: false
+				}
+				this.lists.push(list)
+			},
+			delList(index) {
+				this.lists.splice(index, 1)
+			},
+			hideBtn(index) {
+				this.lists[index].show = false
+			},
+			showBtn(index) {
+				this.lists[index].show = true
+			},
+		}
+	}
+</script>
+
+<style lang="scss">
+	.add-btn {
+		border: none;
+	    width: 50px;
+	    height: 40px;
+	    margin: 10px;
+	    background: red;
+	    color: #fff;
+	    cursor: pointer;
+	}
+	.list,.vuetouch {
+		display: flex;
+		width: 100%;
+		height: 40px;
+		line-height: 40px;
+		margin-bottom: 10px;
+		color: #666;
+		font-size: 14px;
+		background: #eee;
+		transition: all .4s;
+		&.slide-move {
+			transition: transform 1s;
+		}
+		&.slide-enter {
+			transform: translate3d(-100%, 0, 0);
+		}
+		&.slide-leave-active {
+			position: absolute;
+			transform: translate3d(-100%, 0, 0);
+		}
+		&:last-child {
+			margin-bottom: 0;
+		}
+		.del-btn {
+			flex: 0 0 60px;
+			border: none;
+			outline: none;
+			color: #fff;
+			background: red;
+			transition: all .4s;
+			&.move-enter, &.move-leave-active {
+				transform: translate3d(70px, 0, 0);
+			}
+		}
+		.text {
+			flex: 1;
+			padding-left: 20px;
+		}
+	}
+</style>
